fe5570f6ee9460db7d04db290fab0ae902b54d12
32204577 <32204577@qq.com>
2024-10-21 16:40:44 +0800

    Update Main.java and add delay test.

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+         while (true) {
+             try {
+                 System.out.println("Delay 2 seconds.");
+                 Thread.sleep(2000); // 延时2秒（2000毫秒）                                         
+             } catch (InterruptedException e) {
+                 e.printStackTrace();
+             }
+         }
     }
 }
